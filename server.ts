613bd12618b5279b9c15b893e80bea67c20b3ee7
m-essam-s <sir.m.essam.g@gmail.com>
2025-03-10 06:19:27 +0200

    Define create user route

--- server.ts
+++ server.ts
@@ -17,6 +17,16 @@ Bun.serve({
             const user = users.find(user => user.id === Number(req.params.id));
             return user ? Response.json(user) : new Response("User not found", { status: 404 });
         },
+        "/users/create": async req => {
+            const body = await req.json();
+            const { name } = body as { name: string };
+            if (!name) {
+                return new Response("Name is required", { status: 400 });
+            }
+            const newUser = { id: users.length + 1, name };
+            users.push(newUser);
+            return Response.json(newUser, { status: 201 });
+        },
     },
 });
 
