c8758375a897228b942282fe5e1919da1d3e85f9
m-essam-s <sir.m.essam.g@gmail.com>
2025-03-10 06:25:00 +0200

    Define users/:id/update as a dynamic route segmant to update users by id

--- server.ts
+++ server.ts
@@ -27,6 +27,19 @@ Bun.serve({
             users.push(newUser);
             return Response.json(newUser, { status: 201 });
         },
+        "/users/:id/update": async req => {
+            const user = users.find(user => user.id === Number(req.params.id));
+            if (!user) {
+                return new Response("User not found", { status: 404 });
+            }
+            const body = await req.json();
+            const { name } = body as { name: string };
+            if (!name) {
+                return new Response("Name is required", { status: 400 });
+            }
+            user.name = name;
+            return Response.json(user);
+        },
     },
 });
 
